f3f187eea16bd5432e1f0c4e9aa98c5d877e5729
Russell M. Van Tassell <rvantassell@8x8.com>
2018-06-20 16:28:32 -0700

    Added polyfill for PHPUnit_Framework_TestCase (Ref: https://github.com/symfony/symfony/issues/21534)

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -3,11 +3,19 @@
 # Ref: https://phpunit.de/
 #require_once ('PHPUnit/Framework/TestCase.php');
 
-#
-# PHPUnit 3.5 Autoloader
-#  @requires PHPUnit >=3.5
-function testRequiringNewerPHPUnit() {
-  require_once 'PHPUnit/Autoload.php';
+##
+## PHPUnit 3.5 Autoloader
+##  @requires PHPUnit >=3.5
+#function testRequiringNewerPHPUnit() {
+  #require_once 'PHPUnit/Autoload.php';
+#}
+
+// Polyfill PHPUnit 6.0 both ways
+// Ref: https://github.com/symfony/symfony/issues/21534#issuecomment-278278352
+if (!class_exists('\PHPUnit\Framework\TestCase', true)) {
+  class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+} elseif (!class_exists('\PHPUnit_Framework_TestCase', true)) {
+  class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
 }
 
 class HelloWorldTest extends PHPUnit_Framework_TestCase
